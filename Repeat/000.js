83bba142761735120166c78fe9d148e1255a4652
MaxKnyazev <max.knyazev77@gmail.com>
2019-10-02 19:35:22 +0300

    +OOP

--- Repeat/000.js
+++ Repeat/000.js
@@ -1,36 +1,64 @@
 console.log('Started...');
 
-
 let btnCircle = document.querySelector('.content__btnCircle');
 let btnSquare = document.querySelector('.content__btnSquare');
 let contentArea = document.querySelector('.content__area');
 
-function Rectangle (width, height, bgc) {
-    this.width = width;
-    this.height = height;
-    this.bgc = bgc;
-};
+// function Rectangle (width, height, bgc) {
+//     this.width = width;
+//     this.height = height;
+//     this.bgc = bgc;
+// }
+//
+// function Circle (width, height, bgc, borRad) {
+//     Rectangle.call(this, width, height, bgc);
+//     this.borRad = borRad;
+// }
+//
+// Circle.prototype = Object.create(Rectangle.prototype);
+// Circle.prototype.constructor = Circle;
+//
+// Rectangle.prototype.render = function () {
+//     let elem = document.createElement('div');
+//     elem.style.width = this.width + 'rem';
+//     elem.style.height = this.height + 'rem';
+//     elem.style.backgroundColor = this.bgc;
+//
+//     if (this.borRad) {
+//         elem.style.borderRadius = this.borRad + '%';
+//     }
+//
+//     contentArea.append(elem);
+// };
 
-function Circle (width, height, bgc, borRad) {
-    Rectangle.call(this, width, height, bgc);
-    this.borRad = borRad;
-}
-
-Circle.prototype = Object.create(Rectangle.prototype);
-Circle.prototype.constructor = Circle;
-
-Rectangle.prototype.render = function () {
-    let elem = document.createElement('div');
-    elem.style.width = this.width + 'rem';
-    elem.style.height = this.height + 'rem';
-    elem.style.backgroundColor = this.bgc;
-
-    if (this.borRad) {
-        elem.style.borderRadius = this.borRad + '%';
+class Rectangle {
+    constructor (width, height, bgc) {
+        this.width = width;
+        this.height = height;
+        this.bgc = bgc;
     }
 
-    contentArea.append(elem);
-};
+    render () {
+        let elem = document.createElement('div');
+        elem.style.width = this.width + 'rem';
+        elem.style.height = this.height + 'rem';
+        elem.style.backgroundColor = this.bgc;
+
+        if (this.borRad) {
+            elem.style.borderRadius = this.borRad + '%';
+        }
+
+        contentArea.append(elem);
+    }
+}
+
+class Circle extends Rectangle {
+    constructor (width, height, bgc, borRad) {
+        super (width, height, bgc);
+
+        this.borRad = borRad;
+    }
+}
 
 btnSquare.addEventListener('click', function () {
     let square = new Rectangle(4, 4, 'blue');
@@ -45,26 +73,3 @@ btnCircle.addEventListener('click', function () {
 
 
 
-
-/*
-
-btnCircle.addEventListener('click', function () {
-    let circle = document.createElement('div');
-    circle.style.width = '7rem';
-    circle.style.height = '7rem';
-    circle.style.backgroundColor = 'red';
-    circle.style.borderRadius = '50%';
-
-    contentArea.append(circle);
-});
-
-
-btnSquare.addEventListener('click', function () {
-    let square = document.createElement('div');
-    square.style.width = '7rem';
-    square.style.height = '7rem';
-    square.style.backgroundColor = 'blue';
-
-    contentArea.append(square);
-});
-*/
