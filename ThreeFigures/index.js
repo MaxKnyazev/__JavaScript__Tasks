a4348656f5242dbb2102311ace746eb9250ad09a
MaxKnyazev <max.knyazev77@gmail.com>
2019-11-23 13:09:00 +0300

    23.11.19

---
+++ ThreeFigures/index.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  let buttonStart = document.querySelector('.game__start');
+  let buttonAdd = document.querySelector('.game__add');
+  let gameModel = document.querySelector('.game-model');
+  let gameScore = document.querySelector('.game-score');
+  let gameStep = document.querySelector('.game-step');
+
+
+};
+
+
+
+//     let elem = document.createElement('div');
+//     elem.style.width = this.width + 'rem';
+//     elem.style.height = this.height + 'rem';
+//     elem.style.backgroundColor = this.bgc;
+//
+//     if (this.borRad) {
+//         elem.style.borderRadius = this.borRad + '%';
+//     }
+//
+//     contentArea.append(elem);
