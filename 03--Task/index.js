06289c5fb0146ccbce384e363b0ae023aaaa35ee
MaxKnyazev <max.knyazev77@gmail.com>
2019-12-19 18:11:02 +0300

    19.12.2019-18:10

--- 03--Task/index.js
+++ 03--Task/index.js
@@ -2,5 +2,10 @@
 window.onload = function() {
     console.log('Started...');
 
-    
+    let content = document.querySelector('.content');
+    let contentMinus = content.querySelector('.content__minus');
+    let contentNumber = content.querySelector('.content__number');
+    let contentPlus = content.querySelector('.content__plus');
+
+    console.dir(content);
 };
