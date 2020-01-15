831ee18b4914cc6f82fecc7c84d0d2099589d84e
MaxKnyazev <max.knyazev77@gmail.com>
2020-01-15 15:19:21 +0300

    2020--ReactJS first

---
+++ 2020--ReactJS/000/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = (props) => {
+  return (
+    <h1 className = 'Header'>Hello world!</h1>
+  )
+}
+
+// export default function Header (props) {
+//   return (
+//     <h1>Hello world!</h1>
+//   )
+// }
+
+export default Header;
