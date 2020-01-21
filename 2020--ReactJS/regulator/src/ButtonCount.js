cf2b5124464141d5e0ec61977802fa9843ea9ce4
MaxKnyazev <max.knyazev77@gmail.com>
2020-01-21 14:45:18 +0300

    button + image 21/01/2020

--- 2020--ReactJS/regulator/src/ButtonCount.js
+++ 2020--ReactJS/regulator/src/ButtonCount.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './ButtonCount.css';
 
 const ButtonCount = ({sign, clickHandler}) => {
+  console.log(sign);
   return (
-    <button onClick = {clickHandler} className = 'app__button'>{sign}</button>
+    <button onClick = {clickHandler} className = 'app__button'>
+      <img className = 'app__img' src={sign} alt="sign" />
+    </button>
   )
 }
 
--- 2020--ReactJS/regulator/src/Counter.js
+++ 2020--ReactJS/regulator/src/Counter.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Counter.css';
 import ButtonCount from './ButtonCount';
+// import minus from 'logo.svg'
 
 const Counter = ({count, subCount, addCount}) => {
+  // const minus = <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" className="svg-inline--fa fa-plus fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+
+  // console.log('minus= ' + minus)
+  const minus = 'minus.svg';
+  const plus = 'plus.svg';
+
   return (
     <div className = 'app__counter'>
-      <ButtonCount clickHandler = {subCount} sign = '-'/>
+      <ButtonCount clickHandler = {subCount} sign = {minus}/>
       <span>{count}</span>
-      <ButtonCount clickHandler = {addCount} sign = '+'/>
+      <ButtonCount clickHandler = {addCount} sign = {plus}/>
     </div>
   )
 }
