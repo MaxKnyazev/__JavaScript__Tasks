ef045f1cb1c359a2421d8ad42af385c68846bd1b
MaxKnyazev <max.knyazev77@gmail.com>
2020-02-19 19:36:48 +0300

    19.02.2020 redux--html

--- 2020--ReactJS/hsl-color/src/ColorButton.js
+++ 2020--ReactJS/hsl-color/src/ColorButton.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import './ColorButton.css';
+// import './ColorButton.css';
+import 'styled-components/macro';
 
 const ColorButton = ({changeValue, sign}) => {
   return (
-    <button 
+    <button
       className = 'Setting__button' 
       onClick = {changeValue}
+      css={`
+            transition: all 250ms linear;
+            max-width: 130px;
+            max-height: 130px;
+            margin: 0;
+            padding: 0;
+            width: 15vmin;
+            height: 15vmin;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: none;
+
+            & svg {
+              width: 30%;
+              color: rgb(73, 0, 107);
+              transition: all 250ms linear;
+            }
+
+            &:hover {
+              background-color: rgb(175, 89, 255);
+              color: #fff;
+              border-radius: 10px;
+            }
+
+            &:hover svg {
+              color: #fff;
+            }
+      `} 
     >
     {sign}
     </button>
--- 2020--ReactJS/hsl-color/src/ColorValue.js
+++ 2020--ReactJS/hsl-color/src/ColorValue.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import 'styled-components/macro';
+// import 'styled-components/macro';
+import styled from 'styled-components';
 
+const WrapperColorValue = styled.div`
+  background: ${new Date() % 2 === 0 ? 'inherit' : 'blue'};
+`
+const ColorSpan = styled.span`
+  font-size: 5vmax;
+   @media (min-width: 1100px) {
+     font-size: 5rem;
+   }
+`
 const ColorValue = ({value}) => {
   return (
-    <span
-      css={`
-        font-size: 5vmax;
-        /* font-size: ${value/20}vmax; */
-
-        @media (min-width: 1100px) {
-          font-size: 5rem;
-        }
-      `}
-    >
-    {value}
-    </span>
+    <WrapperColorValue>
+      <ColorSpan>{value}</ColorSpan>
+    </WrapperColorValue>
   )
 }
 
 export default ColorValue;
+
+// <span
+// css={`
+//   font-size: 5vmax;
+//   /* font-size: ${value/20}vmax; */
+//   @media (min-width: 1100px) {
+//     font-size: 5rem;
+//   }
+// `}
+// >
+// {value}
+// </span>
